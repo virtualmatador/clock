88fff6be4031a05943ee40ad299f54340cb0bef2
Ali Asadpoor <virtualmatador@yahoo.com>
2024-08-15 19:09:06 -0400

    Allow hiding seconds and disabling 24-hour format

--- src/wall_clock.cpp
+++ src/wall_clock.cpp
@@ -37,24 +37,32 @@ wall_clock::wall_clock()
 	, font_big_{nullptr}
 	, font_medium_{nullptr}
 	, font_small_{nullptr}
-	, volume_{100}
-	, text_color_{255, 255, 255, 255}
-	, background_{0, 0, 0, 255}
 	, display_{0}
 	, width_{0}
 	, height_{0}
+	, digit_width_{0}
+	, colon_width_{0}
+	, ampm_width_{0}
+	, time_width_{0}
 	, frame_time_{std::chrono::system_clock::duration(0)}
 	, now_{0}
 	, tence_{0}
 	, pitch_{0}
+	, volume_{0}
+	, text_color_{0, 0, 0, 0}
+	, background_{0, 0, 0, 0}
 	, dim_{true}
 	, has_chime_{false}
 	, has_alarm_{false}
-	, date_{"%m/%d/%Y"}
+	, date_{}
+	, time_24_{true}
+	, seconds_{true}
 	, next_alarm_{std::size_t(-1)}
 	, audio_device_{0}
 	, texture_second_{nullptr}
 	, size_second_{0, 0}
+	, texture_ampm_{nullptr}
+	, size_ampm_{0, 0}
 	, texture_time_{nullptr}
 	, size_time_{0, 0}
 	, texture_day_{nullptr}
@@ -84,6 +92,7 @@ wall_clock::wall_clock()
 		throw "SDL_RWFromConstMem";
 	}
 	set_window();
+	set_fonts();
 	std::vector<int> sequence(13);
 	std::iota(sequence.begin(), sequence.end(), 0);
 	chimes_.insert(chimes_.end(), sequence.begin(), sequence.end());
@@ -136,35 +145,12 @@ void wall_clock::set_window()
 	{
 		throw "SDL_GetRendererOutputSize";
 	}
-	SDL_RWseek(font_source_, 0, RW_SEEK_SET);
-	TTF_CloseFont(font_big_);
-	font_big_ = TTF_OpenFontRW(font_source_, false, height_ * 4 / 9);
-	if (!font_big_)
-	{
-		throw "TTF_OpenFont";
-	}
-	if (TTF_SizeText(font_big_, "0", &digit_width_, nullptr) < 0 ||
-		TTF_SizeText(font_big_, ":", &colon_width_, nullptr) < 0)
-	{
-		throw "TTF_SizeText";
-	}
-	auto text_width = digit_width_ * 6 + colon_width_ * 2;
-	if (text_width < width_)
-	{
-		text_width = width_;
-	}
-	SDL_RWseek(font_source_, 0, RW_SEEK_SET);
-	TTF_CloseFont(font_big_);
-	font_big_ = TTF_OpenFontRW(font_source_, false, width_ * height_ * 4 / 9 / text_width);
-	if (!font_big_)
-	{
-		throw "TTF_OpenFont";
-	}
-	if (TTF_SizeText(font_big_, "0", &digit_width_, nullptr) < 0 ||
-		TTF_SizeText(font_big_, ":", &colon_width_, nullptr) < 0)
-	{
-		throw "TTF_SizeText";
-	}
+}
+
+void wall_clock::set_fonts()
+{
+	reset_big_font();
+	auto text_width = std::max(digit_width_ * 6 + colon_width_ * 2, width_);
 	SDL_RWseek(font_source_, 0, RW_SEEK_SET);
 	TTF_CloseFont(font_medium_);
 	font_medium_ = TTF_OpenFontRW(font_source_, false, width_ * height_ * 2 / 9 / text_width);
@@ -179,6 +165,50 @@ void wall_clock::set_window()
 	{
 		throw "TTF_OpenFont";
 	}
+	int space_width, a_width, p_width, m_width;
+	if (TTF_SizeText(font_medium_, " ", &space_width, nullptr) < 0 ||
+		TTF_SizeText(font_medium_, "A", &a_width, nullptr) < 0 ||
+		TTF_SizeText(font_medium_, "P", &p_width, nullptr) < 0 ||
+		TTF_SizeText(font_medium_, "M", &m_width, nullptr) < 0)
+	{
+		throw "TTF_SizeText";
+	}
+	ampm_width_ = space_width + std::max(a_width, p_width) + m_width;
+	set_big_font();
+}
+
+void wall_clock::reset_big_font()
+{
+	SDL_RWseek(font_source_, 0, RW_SEEK_SET);
+	TTF_CloseFont(font_big_);
+	font_big_ = TTF_OpenFontRW(font_source_, false, height_ * 4 / 9);
+	if (!font_big_)
+	{
+		throw "TTF_OpenFont";
+	}
+	if (TTF_SizeText(font_big_, "0", &digit_width_, nullptr) < 0 ||
+		TTF_SizeText(font_big_, ":", &colon_width_, nullptr) < 0)
+	{
+		throw "TTF_SizeText";
+	}
+	time_width_ = calculate_time_width();
+}
+
+void wall_clock::set_big_font()
+{
+	SDL_RWseek(font_source_, 0, RW_SEEK_SET);
+	TTF_CloseFont(font_big_);
+	font_big_ = TTF_OpenFontRW(font_source_, false, (width_ - ampm_width_) * height_ * 4 / 9 / (std::max(time_width_, width_) - ampm_width_));
+	if (!font_big_)
+	{
+		throw "TTF_OpenFont";
+	}
+	if (TTF_SizeText(font_big_, "0", &digit_width_, nullptr) < 0 ||
+		TTF_SizeText(font_big_, ":", &colon_width_, nullptr) < 0)
+	{
+		throw "TTF_SizeText";
+	}
+	time_width_ = calculate_time_width();
 }
 
 void wall_clock::create_audio()
@@ -196,6 +226,11 @@ void wall_clock::create_audio()
 	{
 		throw "SDL_create_audio";
 	}
+}
+
+int wall_clock::calculate_time_width()
+{
+    return digit_width_ * 4 + colon_width_ + (seconds_ ? digit_width_ * 2 + colon_width_ : 0) + (time_24_ ? 0 : ampm_width_);
 }
 
 void wall_clock::run()
@@ -266,6 +301,9 @@ void wall_clock::read_config()
 	dim_ = true;
 	has_chime_ = false;
 	has_alarm_ = false;
+	date_ = "%m/%d/%Y";
+	time_24_ = true;
+	seconds_ = true;
 	next_alarm_ = std::size_t(-1);
 	const char* home_directory = getenv(HOME);
 	if (home_directory)
@@ -432,6 +470,36 @@ void wall_clock::read_config()
 					}
 				}
 			}
+			else if (key == "24-hour")
+			{
+				std::string time_24;
+				if (pair_stream >> time_24)
+				{
+					if (time_24 == "true")
+					{
+						time_24_ = true;
+					}
+					else if (time_24 == "false")
+					{
+						time_24_ = false;
+					}
+				}
+			}
+			else if (key == "seconds")
+			{
+				std::string seconds;
+				if (pair_stream >> seconds)
+				{
+					if (seconds == "true")
+					{
+						seconds_ = true;
+					}
+					else if (seconds == "false")
+					{
+						seconds_ = false;
+					}
+				}
+			}
 		}
 		bool will_alarm = false;
 		std::size_t alarm_time = (now_.tm_wday * 24 + now_.tm_hour) * 60 +
@@ -488,10 +556,16 @@ void wall_clock::tick()
 			if (SDL_GetWindowDisplayIndex(wnd_) != display_)
 			{
 				set_window();
+				set_fonts();
+			}
+			else if (time_width_ != calculate_time_width())
+			{
+				reset_big_font();
+				set_big_font();
 			}
 			redraw(false);
 		}
-		else
+		else if (seconds_)
 		{
 			redraw(true);
 		}
@@ -503,23 +577,26 @@ void wall_clock::redraw(const bool second_only)
 {
 	text_color_.a = 255 * (dim_ ? tence_ : 1.0);
 	background_.a = 255 * (dim_ ? tence_ : 1.0);
-	if (SDL_SetRenderDrawColor(renderer_,
-		background_.r, background_.g, background_.b, background_.a) != 0 ||
-		SDL_RenderClear(renderer_) != 0)
+	if (seconds_)
 	{
-		throw "Clear Background";
+		std::stringstream sSecond;
+		sSecond << ":" << std::setfill('0') << std::setw(2) << now_.tm_sec;
+		draw_text(&texture_second_, &size_second_, sSecond.str(), font_big_, text_color_);
 	}
-	std::stringstream sSecond;
-	sSecond << std::setfill('0') << std::setw(2) << now_.tm_sec;
-	draw_text(&texture_second_, &size_second_, sSecond.str(), font_big_, text_color_);
 	if (!second_only)
 	{
 		total_height_ = 0;
 
+		if (!time_24_)
+		{
+			auto ampm = now_.tm_hour < 12 ? " AM" : " PM";
+			draw_text(&texture_ampm_, &size_ampm_, ampm, font_medium_, text_color_);
+		}
+
 		std::stringstream sTime;
-		sTime << std::setfill('0') <<
-			std::setw(2) << now_.tm_hour << ":" <<
-			std::setw(2) << now_.tm_min << ":";
+		sTime <<
+			std::setfill('0') << std::setw(2) << (time_24_ ? now_.tm_hour : now_.tm_hour % 12) << ":" <<
+			std::setfill('0') << std::setw(2) << now_.tm_min;
 		draw_text(&texture_time_, &size_time_, sTime.str(), font_big_, text_color_);
 		total_height_ += size_time_.y;
 
@@ -552,15 +629,38 @@ void wall_clock::redraw(const bool second_only)
 		draw_text(&texture_options_, &size_options_, sInfo.str(), font_small_, text_color_);
 		total_height_ += size_options_.y;
 	}
+	if (SDL_SetRenderDrawColor(renderer_,
+		background_.r, background_.g, background_.b, background_.a) != 0 ||
+		SDL_RenderClear(renderer_) != 0)
+	{
+		throw "Clear Background";
+	}
+	int iX;
 	int iY = (height_ - total_height_) / 5;
-	render_texture(texture_second_, &size_second_, width_ / 2 + digit_width_ * 2 + colon_width_, iY);
-	render_texture(texture_time_, &size_time_, width_ / 2 - digit_width_, iY);
+
+	iX = (width_ - size_time_.x - (seconds_ ? size_second_.x : 0) - (time_24_ ? 0 : size_ampm_.x)) / 2;
+	render_texture(texture_time_, &size_time_, iX, iY);
+	iX += size_time_.x;
+	if (seconds_)
+	{
+		render_texture(texture_second_, &size_second_, iX, iY);
+		iX += size_second_.x;
+	}
+	if (!time_24_)
+	{
+		render_texture(texture_ampm_, &size_ampm_, iX, iY + (size_time_.y - size_ampm_.y) / 2);
+		iX += size_ampm_.x;
+	}
 	iY += size_time_.y + (height_ - total_height_) / 5;
-	render_texture(texture_day_, &size_day_, width_ / 2, iY);
+	iX = (width_ - size_day_.x) / 2;
+	render_texture(texture_day_, &size_day_, iX, iY);
 	iY += size_day_.y + (height_ - total_height_) / 5;
-	render_texture(texture_date_, &size_date_, width_ / 2, iY);
+	iX = (width_ - size_date_.x) / 2;
+	render_texture(texture_date_, &size_date_, iX, iY);
 	iY += size_date_.y + (height_ - total_height_) / 5;
-	render_texture(texture_options_, &size_options_, width_ / 2, iY);
+	iX = (width_ - size_options_.x) / 2;
+	render_texture(texture_options_, &size_options_, iX, iY);
+	iY += size_date_.y + (height_ - total_height_) / 5;
 	SDL_RenderPresent(renderer_);
 }
 
@@ -586,7 +686,7 @@ void wall_clock::draw_text(SDL_Texture** texture, SDL_Point* size, const std::st
 
 void wall_clock::render_texture(SDL_Texture* texture, const SDL_Point* size, const int x, const int y)
 {
-	SDL_Rect dest{x - size->x / 2, y, size->x, size->y};
+	SDL_Rect dest{x, y, size->x, size->y};
 	if (SDL_RenderCopy(renderer_, texture, nullptr, &dest) != 0)
 	{
 		throw "SDL_RenderCopy";
